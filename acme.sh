a5b0b497de2ff4686a8cf73867d660df3be08458
zytakeshi <zyvista@gmail.com>
2024-06-27 16:36:09 +0900

    Create acme.sh

---
+++ acme.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+export LANG=en_US.UTF-8
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN="\033[0m"
+
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+realip(){
+    ipv4=$(curl -s4m8 ip.sb -k)
+    ipv6=$(curl -s6m8 ip.sb -k)
+}
+
+# 判断系统及定义系统安装依赖方式
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora")
+PACKAGE_UPDATE=("apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "yum -y install")
+
+[[ $EUID -ne 0 ]] && red "注意: 请在root用户下运行脚本" && exit 1
+
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && red "目前暂不支持你的VPS的操作系统！" && exit 1
+
+if [[ -z $(type -P curl) ]]; then
+    if [[ ! $SYSTEM == "CentOS" ]]; then
+        ${PACKAGE_UPDATE[int]}
+    fi
+    ${PACKAGE_INSTALL[int]} curl
+fi
+
+acme_cert(){
+    cert_path="/root/cert.crt"
+    key_path="/root/private.key"
+
+    chmod a+x /root # 让 Hysteria 主程序访问到 /root 目录
+
+    WARPv4Status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    WARPv6Status=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
+    if [[ $WARPv4Status =~ on|plus ]] || [[ $WARPv6Status =~ on|plus ]]; then
+        wg-quick down wgcf >/dev/null 2>&1
+        systemctl stop warp-go >/dev/null 2>&1
+        realip
+        wg-quick up wgcf >/dev/null 2>&1
+        systemctl start warp-go >/dev/null 2>&1
+    else
+        realip
+    fi
+    
+    read -p "请输入需要申请证书的域名：" domain
+    [[ -z $domain ]] && red "未输入域名，无法执行操作！" && exit 1
+    green "已输入的域名：$domain" && sleep 1
+
+    domainIP=$(dig +short $domain)
+    if [[ $domainIP == $ipv4 || $domainIP == $ipv6 ]]; then
+        ${PACKAGE_INSTALL[int]} curl wget sudo socat openssl
+        if [[ $SYSTEM == "CentOS" ]]; then
+            ${PACKAGE_INSTALL[int]} cronie
+            systemctl start crond
+            systemctl enable crond
+        else
+            ${PACKAGE_INSTALL[int]} cron
+            systemctl start cron
+            systemctl enable cron
+        fi
+        curl https://get.acme.sh | sh -s email=$(date +%s%N | md5sum | cut -c 1-16)@gmail.com
+        source ~/.bashrc
+        bash ~/.acme.sh/acme.sh --upgrade --auto-upgrade
+        bash ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+        if [[ -n $(echo $ipv6 | grep ":") ]]; then
+            bash ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --listen-v6 --insecure
+        else
+            bash ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --insecure
+        fi
+        bash ~/.acme.sh/acme.sh --install-cert -d ${domain} --key-file /root/private.key --fullchain-file /root/cert.crt --ecc
+        if [[ -f /root/cert.crt && -f /root/private.key ]] && [[ -s /root/cert.crt && -s /root/private.key ]]; then
+            echo $domain > /root/ca.log
+            sed -i '/--cron/d' /etc/crontab >/dev/null 2>&1
+            echo "0 0 * * * root bash /root/.acme.sh/acme.sh --cron -f >/dev/null 2>&1" >> /etc/crontab
+            green "证书申请成功! 脚本申请到的证书 (cert.crt) 和私钥 (private.key) 文件已保存到 /root 文件夹下"
+            yellow "证书crt文件路径如下: /root/cert.crt"
+            yellow "私钥key文件路径如下: /root/private.key"
+        fi
+    else
+        red "当前域名解析的IP与当前VPS使用的真实IP不匹配"
+        green "建议如下："
+        yellow "1. 请确保CloudFlare小云朵为关闭状态(仅限DNS), 其他域名解析或CDN网站设置同理"
+        yellow "2. 请检查DNS解析设置的IP是否为VPS的真实IP"
+        yellow "3. 脚本可能跟不上时代, 建议截图发布到GitHub Issues、GitLab Issues、论坛或TG群询问"
+        exit 1
+    fi
+}
+
+acme_cert
